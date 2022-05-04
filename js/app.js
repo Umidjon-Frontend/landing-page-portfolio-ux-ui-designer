a7b25a73ea33a6b85d0ccecbb88ec352e7e903c7
Umidjon-Fullstack <umidjonyusufov14011999@gmail.com>
2022-05-04 12:58:11 +0500

    finished

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 // VARIABLES
+const btnMenu = document.querySelector(".btn-menu");
+const nav = document.querySelector(".header-inner .nav");
+
+btnMenu.addEventListener("click", () => {
+    btnMenu.classList.toggle("active");
+    nav.classList.toggle("toggle-active");
+});
